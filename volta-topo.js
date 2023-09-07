40d56ff10b4ba1830eab32972d5a4a47b0e52919
Toim Nasi <86308977+AntonioNasi@users.noreply.github.com>
2023-09-07 17:50:04 -0300

    Adjustments

---
+++ volta-topo.js
@@ -0,0 +1,17 @@
+let mybutton = document.getElementById("myBtnTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
