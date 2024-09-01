98898a00b4868f15cfa01c18bb51cb7804cd9ac2
Toim Nasi <86308977+AntonioNasi@users.noreply.github.com>
2024-09-01 12:03:51 -0300

    Js

---
+++ js/mudaCapa.js
@@ -0,0 +1,7 @@
+
+function outraImagem(){
+
+    var img = document.getElementById("m-img").style.backgroundImage;
+    
+    img.style.backgroundImage = "url(https://img.freepik.com/free-photo/person-holding-light-bulb-with-graduation-cap_23-2148721299.jpg)"
+}
